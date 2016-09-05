ab4a2c85bb45a68a02e901c419988e3d923c7d5b
CC-Cai <326791352@qq.com>
2016-09-05 18:29:46 +0800

    BackBone/  相关修改

--- BackBone/Backbone-serverTest/routes/backbone.js
+++ BackBone/Backbone-serverTest/routes/backbone.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET collections page. */
+router.get('/collections', function(req, res, next) {
+  res.render('backbone/collections', { title: 'Express' });
+});
+
+router.get('/bookShelf',function(req,res,next){
+	var bookShelf=[];
+	var book1 ={title:"book1"};
+	var book2 ={title:"book2"};
+	var book3 ={title:"book3"};
+	bookShelf.push(book1);
+	bookShelf.push(book2);
+	bookShelf.push(book3);
+	
+	setTimeout(function(){ 
+		res.send(bookShelf);
+	},2000);
+});
+
 module.exports = router;
